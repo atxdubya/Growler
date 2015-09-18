50e468507e51f2368e7b578fc8a09713d5f6d3ad
mapineda Pineda <mapineda1222@gmail.com>
2015-09-18 16:07:56 -0500

    add @growl_feed to index

--- app.rb
+++ app.rb
@@ -1,27 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
-
 before do
 	# Initiating the growl_feed
 	session[:growl_feed] ||= []	
 end
-
 enable :sessions
-
 # Routes
-
-
 # Essentially homepage
 get '/' do
+	@growl_feed = session[:growl_feed]
 	erb :feed
 end
-
 # Posting the growl input to the feed
 post '/feed' do
-
 	session[:growl_feed] << params[:growl]
 	@growl_feed = session[:growl_feed]
-
 	erb :feed
 end
 
